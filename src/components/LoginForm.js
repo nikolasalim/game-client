1189c8b601add95369559061f13e9749a146d44b
nikolasalim <nikolasalim@gmail.com>
2020-03-09 14:16:00 +0100

    set up folder structure and add user reducer and sign up component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class LoginForm extends React.Component {
+  render() {
+    return <div>Log in</div>;
+  }
+}
+
+export default LoginForm;
---
+++ src/components/SignupForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SignupForm extends React.Component {
+  render() {
+    return <div>Sign up</div>;
+  }
+}
+
+export default SignupForm;
